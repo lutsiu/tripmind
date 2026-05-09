70b9d6a73e0fc7f14c8fbc3129f362749bd39d01
lutsiu <oleksandr.lutsiuk.1603@gmail.com>
2026-05-09 13:54:16 +0200

    First commit

---
+++ app/core/config.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+ROOT_DIR = Path(__file__).parent.parent.parent
+
+APP_NAME = os.getenv("APP_NAME", "TripMind")
+ENV = os.getenv("ENV", "dev")
+
+STORAGE_DIR = ROOT_DIR / "storage"
+TRIP_STORAGE_DIR = STORAGE_DIR / "trips"
